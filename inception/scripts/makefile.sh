8a0430f9fd3ff95f79cde7884f6f6b69f96a7d92
danielfdez17 <daniel17dev@gmail.com>
2026-02-18 17:17:03 +0100

    test

--- inception/scripts/makefile.sh
+++ inception/scripts/makefile.sh
@@ -7,28 +7,28 @@ all: build up
 
 down:
 	@echo "Stopping and removing containers, networks, volumes, and images..."
-    docker-compose -f srcs/docker-compose.yml down
+	docker-compose -f srcs/docker-compose.yml down
 
 build:
-    @echo "Building Docker images without cache..."
-    docker-compose -f srcs/docker-compose.yml build --no-cache
+	@echo "Building Docker images without cache..."
+	docker-compose -f srcs/docker-compose.yml build --no-cache
 
 up: build
-    @echo "Starting containers in detached mode..."
-    docker-compose -f srcs/docker-compose.yml up
+	@echo "Starting containers in detached mode..."
+	docker-compose -f srcs/docker-compose.yml up
 
 clean:
-    @echo "Pruning Docker system to remove all unused data..."
-    docker system prune -a --volumes -f
+	@echo "Pruning Docker system to remove all unused data..."
+	docker system prune -a --volumes -f
 
 st:
-    @echo "Listing Docker images and running containers..."
-    docker images
-    docker ps
+	@echo "Listing Docker images and running containers..."
+	docker images
+	docker ps
 
 kill:
-    @echo "Killing all running containers..."
-    docker-compose -f srcs/docker-compose.yml down --rmi all --volumes --remove-orphans
+	@echo "Killing all running containers..."
+	docker-compose -f srcs/docker-compose.yml down --rmi all --volumes --remove-orphans
 
 re: down kill clean all
 
