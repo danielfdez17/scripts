728bfc39907c82b39626f9ec411a7923102b1c41
danfern3 <danfern3@student.42madrid.com>
2026-02-18 09:21:28 +0100

    feat: add use of zsh when installing inception dependencies

--- inception/install.sh
+++ inception/install.sh
@@ -10,6 +10,9 @@ echo "Installing zsh..."
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
+sudo echo >> ~/.bashrc
+sudo echo >> ~/.bashrc
+sudo echo "zsh" >> ~/.bashrc
 
 echo "Installing Visual Studio Code..."
 sudo snap install --classic code
