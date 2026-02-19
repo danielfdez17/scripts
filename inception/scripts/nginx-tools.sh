fcb9a107c11528bc2cdd9370abc5a520216cd9f6
danielfdez17 <daniel17dev@gmail.com>
2026-02-19 12:15:11 +0100

    fix: issue with mkdir

--- inception/scripts/nginx-tools.sh
+++ inception/scripts/nginx-tools.sh
@@ -6,21 +6,21 @@ cat << EOF
 
 # Folder where certificates will be stored
 SSL_DIR="/etc/nginx/ssl"
-mkdir -p $SSL_DIR
+mkdir -p /etc/nginx/ssl
 
 # If the certificate and key already exist, skip generation to avoid overwriting
-if [ -f "\$SSL_DIR/nginx.crt" ] && [ -f "\$SSL_DIR/nginx.key" ]; then
+if [ -f "/etc/nginx/ssl/nginx.crt" ] && [ -f "/etc/nginx/ssl/nginx.key" ]; then
     echo "SSL certificates already exist. Skipping generation."
 else
 
     echo "Generating SSL certificate..."
 
     # Generate a self-signed certificate
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout \$SSL_DIR/nginx.key -out \$SSL_DIR/nginx.crt -subj "/C=ES/ST=Madrid/L=Madrid/O=42Madrid/OU=Student/CN=danfern3.42.fr"
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=ES/ST=Madrid/L=Madrid/O=42Madrid/OU=Student/CN=danfern3.42.fr"
 
     # Set appropriate permissions for the certificate and key
-    chmod 644 \$SSL_DIR/nginx.crt
-    chmod 600 \$SSL_DIR/nginx.key
+    chmod 644 /etc/nginx/ssl/nginx.crt
+    chmod 600 /etc/nginx/ssl/nginx.key
 
     echo "SSL certificate generated successfully!"
 
