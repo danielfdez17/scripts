78c91922e32d06023dfc9ad616ff794c9b48264d
danielfdez17 <daniel17dev@gmail.com>
2026-02-19 12:30:23 +0100

    fix

--- inception/scripts/nginx-conf.sh
+++ inception/scripts/nginx-conf.sh
@@ -41,7 +41,7 @@ server {
 	index index.php index.html;
 
 	location / {
-		try_files $uri $uri/ /index.php?$args =404;
+		try_files \$uri \$uri/ /index.php?\$args;
 	}
 
 	location ~ \.php$ {
@@ -49,9 +49,9 @@ server {
 		fastcgi_index index.php;
 
 		include fastcgi_params;
-		fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+		fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
 		fastcgi_split_path_info ^(.+?\.php)(/.*)$;
-		fastcgi_param PATH_INFO $fastcgi_path_info;
+		fastcgi_param PATH_INFO \$fastcgi_path_info;
 		
 	}
 }
