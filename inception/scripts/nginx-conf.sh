816dc170e66ca12771ca9019bc05110948e71e2e
danielfdez17 <daniel17dev@gmail.com>
2026-02-19 12:19:18 +0100

    fix: nginx.conf

--- inception/scripts/nginx-conf.sh
+++ inception/scripts/nginx-conf.sh
@@ -47,9 +47,10 @@ server {
 	location ~ \.php$ {
 		fastcgi_pass wp-php:9000;
 		fastcgi_index index.php;
+		include snippets/fastcgi-php.conf;
 
-		include fastcgi_params;
-		fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+		# include fastcgi_params;
+		# fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
 		# fastcgi_split_path_info ^(.+?\.php)(/.*)$;
 		# fastcgi_param PATH_INFO $fastcgi_path_info;
 		
