b1d1a7088f90f946b01db8e5f94b1159edffb1c0
danielfdez17 <daniel17dev@gmail.com>
2026-02-19 12:11:28 +0100

    fix: issue when starting nginx if ssl certificates already exists

--- inception/scripts/nginx-tools.sh
+++ inception/scripts/nginx-tools.sh
@@ -11,19 +11,20 @@ mkdir -p $SSL_DIR
 # If the certificate and key already exist, skip generation to avoid overwriting
 if [ -f "\$SSL_DIR/nginx.crt" ] && [ -f "\$SSL_DIR/nginx.key" ]; then
     echo "SSL certificates already exist. Skipping generation."
-    exit 0
+else
+
+    echo "Generating SSL certificate..."
+
+    # Generate a self-signed certificate
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout \$SSL_DIR/nginx.key -out \$SSL_DIR/nginx.crt -subj "/C=ES/ST=Madrid/L=Madrid/O=42Madrid/OU=Student/CN=danfern3.42.fr"
+
+    # Set appropriate permissions for the certificate and key
+    chmod 644 \$SSL_DIR/nginx.crt
+    chmod 600 \$SSL_DIR/nginx.key
+
+    echo "SSL certificate generated successfully!"
+
 fi
-
-echo "Generating SSL certificate..."
-
-# Generate a self-signed certificate
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout \$SSL_DIR/nginx.key -out \$SSL_DIR/nginx.crt -subj "/C=ES/ST=Madrid/L=Madrid/O=42Madrid/OU=Student/CN=danfern3.42.fr"
-
-# Set appropriate permissions for the certificate and key
-chmod 644 \$SSL_DIR/nginx.crt
-chmod 600 \$SSL_DIR/nginx.key
-
-echo "SSL certificate generated successfully!"
 
 nginx -g "daemon off;"
 EOF
