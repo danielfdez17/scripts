993272bae8230e15e2a30b19e6016749551c0a6c
danielfdez17 <daniel17dev@gmail.com>
2026-03-13 16:27:38 +0100

    feat(git aliases): Add git aliases inside a function

--- git.sh
+++ git.sh
@@ -8,13 +8,16 @@ else
     echo "Git is installed."
 fi
 
-# Git aliases
-echo "Setting up Git aliases..."
-alias gss='git status -s'
-alias ga='git add'
-alias gc='git commit -m'
-alias gp='git push'
-alias gl='git log --oneline --graph --decorate'
-alias gpr='git pull --rebase'
-alias gls='git ls-files'
-alias grv='git remote -v'
+setup_git_aliases() {
+    echo "Setting up Git aliases..."
+    alias gss='git status -s' \
+    && alias ga='git add' \
+    && alias gc='git commit -m' \
+    && alias gp='git push' \
+    && alias gl='git log --oneline --graph --decorate' \
+    && alias gpr='git pull --rebase' \
+    && alias gls='git ls-files' \
+    && alias grv='git remote -v'
+}
+
+setup_git_aliases
