92133bd7075903cdbbe7c7e057f0a6b4dc70f6c4
danielfdez17 <daniel17dev@gmail.com>
2026-03-13 16:22:31 +0100

    feat(git): Remove exit from conditional

--- git.sh
+++ git.sh
@@ -4,12 +4,12 @@
 if ! command -v git &> /dev/null
 then
     echo "Git is not installed. You should install Git in order to use the git aliases."
-    exit 1
 else
     echo "Git is installed."
 fi
 
 # Git aliases
+echo "Setting up Git aliases..."
 alias gss='git status -s'
 alias ga='git add'
 alias gc='git commit -m'
