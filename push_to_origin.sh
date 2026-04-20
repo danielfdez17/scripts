e74c683d47f8e2924af69774b86ebc466084acbd
danielfdez17 <daniel17dev@gmail.com>
2026-04-20 12:29:31 +0200

    feat(script): Add script to push a new branch and set the origin tracking

---
+++ push_to_origin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+GREEN="\033[32m"
+RED="\033[31m"
+YELLOW="\033[33m"
+RESET="\033[0m"
+OK="$GREEN🗸$RESET"
+ERROR="$RED✗$RESET"
+WARNING="$YELLOW⚠$RESET"
+
+current_branch=$(git branch --show-current)
+
+echo "Pushing $current_branch to origin..."
+git push -u origin "$current_branch" || { echo -e "$ERROR Failed to push $current_branch to origin. Please ensure you have permission and try again."; exit 1; }
+
+echo -e "$OK Successfully pushed $current_branch to origin."
