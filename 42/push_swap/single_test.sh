4b6615356d42afaeeb55cb6c247764413c945446
danielfdez17 <daniel17dev@gmail.com>
2026-05-06 18:29:18 +0200

    fix(linter): Solve linter error related to single quotes

--- 42/push_swap/single_test.sh
+++ 42/push_swap/single_test.sh
@@ -5,7 +5,7 @@ make bonus
 if [ -z "$1" ]; then
 	print_warning "Usage: $0 <numbers>"
 	print_warning "Run:"
-	echo 'bash ./vendor/scripts/42/push_swap/single_test.sh $(max=100;seq -"$max" "$max" | shuf | head -n "$max" | tr "\n" " ")'
+	echo "bash ./vendor/scripts/42/push_swap/single_test.sh $(max=100;seq -"$max" "$max" | shuf | head -n "$max" | tr "\n" " ")"
 	# print_info "Example: $0 \"3 2 1\""
 	exit 1
 fi
