4a86670574872b7cb22508de0a58940097cb8db2
danielfdez17 <daniel17dev@gmail.com>
2026-05-30 07:01:25 +0200

    feat: Move ensure-env script to root folder and now uses the utils file

---
+++ ensure-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+. "$(dirname "$0")/utils.sh"
+
+files=$(find . -maxdepth 1 -name ".env" -type f)
+
+print_info "Checking for .env files..."
+
+if [ -z "$files" ]; then
+  print_error "No .env file found. Please create a .env file with the necessary environment variables."
+  exit 1
+fi
+
+print_ok " .env file found."
